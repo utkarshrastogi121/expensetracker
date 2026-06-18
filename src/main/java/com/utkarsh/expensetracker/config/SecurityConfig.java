62a6f435c8da08edeee89f6c28f8b463a753c476
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2026-06-18 22:43:44 +0530

    added deployed ui link for cors

--- src/main/java/com/utkarsh/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/utkarsh/expensetracker/config/SecurityConfig.java
@@ -81,13 +81,20 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://expensetrack-ui.vercel.app"
+        ));
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+
         configuration.setAllowedHeaders(List.of("*"));
+
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 
